d430894bcaee61b615765d762f6cfc97c732b5d2
siMobin <shakibulislammobin@gmail.com>
2025-09-01 18:03:16 +0600

    Update dependabot

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" />
-        <link rel="shortcut icon" href="/images/logo.png" type="image/x-icon" />
+        {/* <link rel="shortcut icon" href="images/logo.png" type="image/x-icon" /> */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
